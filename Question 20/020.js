43e427c855749e33a6e9664a132557687336ab4a
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 22:15:08 +0500

    Add files via upload

---
+++ Question 20/020.js
@@ -0,0 +1,5 @@
+// 20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything
+// else you’d like. Write a program that creates a list containing these items.
+var favorit = ["pakistan", "dubai", "jarmany", "turkey"];
+console.log("list of countries");
+favorit.map(function (as) { return console.log(as); });
---
+++ Question 20/020.ts
@@ -0,0 +1,8 @@
+// 20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything
+// else you’d like. Write a program that creates a list containing these items.
+
+let favorit: string[]= ["pakistan","dubai","jarmany","turkey"]
+
+console.log("list of countries");
+
+favorit.map((item)=>console.log(item));
